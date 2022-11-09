53a7e4b93e233894747559c33fbe5699b468263c
willianlim <willianrosendodelima@outlook.com>
2022-11-09 07:21:08 -0300

    creating vectors

---
+++ javaEssencial/src/Program16.java
@@ -0,0 +1,15 @@
+/*
+ * Vectors part 1
+ */
+
+public class Program16 {
+    public static void main(String[] args) {
+        int numbers[] = new int[5];
+        int others_numbers[] = {1, 2, 3, 4, 5};
+        float values[] = new float[5];
+        char characters[] = new char[20];
+        String names[] = new String[4];
+
+        String fruits[] = {"Manga", "Uva", "Banana"};
+    }
+}
