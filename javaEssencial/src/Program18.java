e4a158d5c4b273d2f45a6f2826586ced6967446a
willianlim <willianrosendodelima@outlook.com>
2022-11-09 07:47:08 -0300

    Introdcution to matrices

---
+++ javaEssencial/src/Program18.java
@@ -0,0 +1,17 @@
+/*
+ *
+ */
+
+public class Program18 {
+    public static void main(String[] args) {
+        int[][] others_numbers;
+        int[][] numbers = new int[3][3];
+        int[][] more_numbers = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+
+        int[][] matrices = new int[2][];
+        matrices[0] = new int[5];
+        matrices[1] = new int[3];
+
+        int[][] new_matrices = {{1, 2}, {4, 5, 6, 7, 8}, {9, 10, 11}};
+    }
+}
