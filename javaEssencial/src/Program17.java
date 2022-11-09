3f4b64872294cc7b8305a08705b8b76a5bcbfafb
willianlim <willianrosendodelima@outlook.com>
2022-11-09 07:36:35 -0300

    one-dimensional data structure

---
+++ javaEssencial/src/Program17.java
@@ -0,0 +1,34 @@
+/*
+ * Vectors part 2
+ */
+
+public class Program17 {
+
+    public static void main(String[] args) {
+        int[] numbers = new int[10];
+
+        System.out.println("Vector size: " + numbers.length);
+
+        for (int i = 0; i < numbers.length; i++) {
+            numbers[i] = i + 3;
+        }
+
+        System.out.println(numbers[0]);
+        System.out.println(numbers[9]);
+
+        for (int i = 0; i < numbers.length; i++) {
+            numbers[i] = (int)Math.round(Math.random() * 10);
+        }
+
+        System.out.println(numbers[0]);
+        System.out.println(numbers[9]);
+
+        System.out.println(Math.random());
+        System.out.println(Math.random() * 10);
+        System.out.println(Math.round(Math.random() * 10));
+
+        for (int i: numbers) {
+            System.out.println(i);
+        }
+    }
+}
