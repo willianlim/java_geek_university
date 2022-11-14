8bf0e8db28038efb7b1341fc1cb55a5c5304b4d8
willianlim <willianrosendodelima@outlook.com>
2022-11-14 08:40:34 -0300

    performing calculations with Math

---
+++ javaEssencial/src/br/com/geekuniversity/section18/Program40.java
@@ -0,0 +1,18 @@
+package br.com.geekuniversity.section18;
+
+public class Program40 {
+    public static void main(String[] args) {
+        System.out.println(Math.E);
+        System.out.println(Math.PI);
+        System.out.println(Math.sin(45));
+        System.out.println(Math.cos(45));
+        System.out.println(Math.tan(45));
+        System.out.println(Math.round(Math.PI));
+        System.out.println(Math.ceil(Math.PI));
+        System.out.println(Math.floor(Math.PI));
+        System.out.println(Math.pow(3, 2));
+        System.out.println(Math.round(Math.pow(3, 2)));
+        System.out.println(Math.floor(Math.pow(3, 2)));
+        System.out.println(Math.sqrt(9));
+    }
+}
